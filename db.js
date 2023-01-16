70610d8103d788d1d9158777d43ea6f05678ec08
ABHINAV SHUKLA <80741439+AmitaKumari1994@users.noreply.github.com>
2023-01-16 22:54:10 +0530

    Update db.js
    connecting to mongo atlas db migration to online
    
    Signed-off-by: ABHINAV SHUKLA <80741439+AmitaKumari1994@users.noreply.github.com>

--- db.js
+++ db.js
@@ -1,10 +1,29 @@
  const mongoose = require('mongoose');
- const mongoURI = `mongodb+srv://abhiscience:Mongo1234@abhinavcluster.b2daqtw.mongodb.net/abhiKart`;
+//  const mongoURI = `mongodb://127.0.0.1:27017/abhiKart`;
+ const mongoURI =`mongodb+srv://abhiscience:Mongo1234@abhinavcluster.b2daqtw.mongodb.net/abhiKart?retryWrites=true&w=majority`
 
+
+ const connectionParams ={
+    useNewUrlParser :true,
+    useUnifiedTopology:true
+
+ }
  const connectToMongo = ()=>{
-     mongoose.connect(mongoURI , ()=>{
-         console.log("Connected to mongoose successfully");
+     mongoose.connect(mongoURI ,connectionParams).then(()=>{
+        console.info("connected to db");
+     }).catch((e)=>{
+        console.log("error")
      })
  }
 
  module.exports = connectToMongo;
+//  const mongoose = require('mongoose');
+//  const mongoURI = `mongodb+srv://abhiscience:Mongo1234@abhinavcluster.b2daqtw.mongodb.net/abhiKart`;
+
+//  const connectToMongo = ()=>{
+//      mongoose.connect(mongoURI , ()=>{
+//          console.log("Connected to mongoose successfully");
+//      })
+//  }
+
+//  module.exports = connectToMongo;
