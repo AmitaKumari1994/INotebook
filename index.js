4b18083d074b18cfc09aea5f0c155571a63e8e28
ABHINAV SHUKLA <80741439+AmitaKumari1994@users.noreply.github.com>
2023-01-23 16:30:09 +0530

    get routes enables
    Signed-off-by: ABHINAV SHUKLA <80741439+AmitaKumari1994@users.noreply.github.com>

--- index.js
+++ index.js
@@ -19,9 +19,9 @@
  app.use('/api/auth',require('./routes/auth'))
  app.use('/api/notes',require('./routes/notes'))
 
-//  app.get('/',(req,res) => {
-//      res.send("Hello world");
-//  })
+  app.get('/',(req,res) => {
+      res.send("Hello world");
+  })
 
  app.listen(port, ()=>{
      console.log(`Example app listening at http://localhost:${port}`)
